f545e0a6ea152650d45f95ee41321eaf0d9d46c4
Kostiantyn Perun <68146217+Perun108@users.noreply.github.com>
2023-03-04 02:01:57 +0100

    Update 1880-check-if-word-equals-summation-of-two-words.py

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -4,3 +4,12 @@ class Solution:
         return int("".join([str(mapp.index(el)) for el in firstWord])) + int(
             "".join([str(mapp.index(el)) for el in secondWord])
         ) == int("".join([str(mapp.index(el)) for el in targetWord]))
+
+###
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        mapp = "abcdefghij"
+        first_value = "".join([str(mapp.index(el)) for el in firstWord])
+        second_value = "".join([str(mapp.index(el)) for el in secondWord])
+        third_value = "".join([str(mapp.index(el)) for el in targetWord])
+        return int(first_value) + int(second_value) == int(third_value)
