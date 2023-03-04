befd05016164657595888d0d77aecc9b70420deb
Kostiantyn Perun <68146217+Perun108@users.noreply.github.com>
2023-03-04 01:45:45 +0100

    Update 1812-determine-color-of-a-chessboard-square.py

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -1,3 +1,9 @@
 class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
         return bool((ord(coordinates[0]) % 2) ^ (int(coordinates[1]) % 2))
+    
+###
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        return ord(coordinates[0]) % 2 == 1 and int(coordinates[1]) % 2 == 0 
+            or ord(coordinates[0]) % 2 == 0 and int(coordinates[1]) % 2 == 1
