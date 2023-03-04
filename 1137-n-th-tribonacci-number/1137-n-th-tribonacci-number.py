6829303bdf048296764f51bf39f65afc5118a74b
Kostiantyn Perun <68146217+Perun108@users.noreply.github.com>
2023-03-04 03:21:48 +0200

    Time: 35 ms (33.85%), Space: 13.9 MB (6.98%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        res = [0, 1, 1]
+        for i in range(3, n+2):
+            res.append(res[i - 3] + res[i - 2] + res[i - 1])
+        return res[n]
