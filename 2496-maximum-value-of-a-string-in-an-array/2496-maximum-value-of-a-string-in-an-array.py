0eccdb99fa27b7e6a0eb0ad85c12398c17c54447
Kostiantyn Perun <68146217+Perun108@users.noreply.github.com>
2023-03-07 03:05:05 +0200

    Time: 33 ms (74.62%), Space: 13.9 MB (48.65%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.py
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.py
@@ -2,12 +2,8 @@ class Solution:
     def maximumValue(self, strs: List[str]) -> int:
         max_ = 0
         for el in strs:
-            if el.isdigit():
-                curr = int(el)
-            else:
-                curr = len(el)
-            if curr > max_:
-                max_ = curr
+            curr = int(el) if el.isdigit() else len(el)
+            max_ = curr if curr > max_ else max_
         return max_
             
                 
