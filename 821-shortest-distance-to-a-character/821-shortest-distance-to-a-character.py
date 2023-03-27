1bbb9c288f52e2db676f35a2e615481ca0fabde8
Kostiantyn Perun <68146217+Perun108@users.noreply.github.com>
2023-03-26 21:22:55 -0400

    Time: 43 ms (68.17%), Space: 13.8 MB (87.97%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -4,9 +4,6 @@ class Solution:
         c_indexes = sorted([i for i, el in enumerate(s) if el == c])
         closest_ind = 0
         for i, el in enumerate(s):
-            # if el == c:
-            #     answer.append(0)
-            # else:
             if (closest_ind < len(c_indexes) - 1) and abs(i - c_indexes[closest_ind]) > abs(i - c_indexes[closest_ind + 1]):
                 closest_ind += 1
             answer.append(abs(i - c_indexes[closest_ind]))
